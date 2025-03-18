24eb068ce8e4cdc10b1b4e1fb0fedfaaa4d78d05
Quan Huynh <152340025+quanhuynh11@users.noreply.github.com>
2025-03-17 19:52:06 -0600

    home and translate pages

---
+++ app/translate-page/page.js
@@ -0,0 +1,11 @@
+export default function TranslatePage() {
+    return (
+        <section className="w-screen h-screen bg-slate-900 p-5 text-center">
+            <h1 className="text-4xl font-bold">Translate Page</h1>
+            <p className="text-2xl">Welcome to the Real-time Translation App</p>
+            <section className="mt-5">
+                <button className="bg-blue-600 text-white py-2 px-4 rounded-lg text-2xl hover:cursor-pointer hover:bg-blue-800">Translate!</button>
+            </section>
+        </section>
+    )
+}
